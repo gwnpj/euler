6f512260f1c0f97f9cac8ee8c27297ae160a7533
paul.patault <p.patault@gmail.com>
2018-10-05 22:38:01 +0200

    x

--- General/P6.01.py
+++ General/P6.01.py
@@ -13,3 +13,11 @@ print ('carré de la somme', cs)
 dif = cs-sc
 
 print ('\nDifférence "la somme des carrés" - "le carré de leur somme" est', dif )
+
+
+
+while i < int(len(n)-1):
+    if int(n[i])*int(n[i+1]) > a:
+        a = int(n[i])*int(n[i+1])
+    i += 1
+print ("\n", a)
