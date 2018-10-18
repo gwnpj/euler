7a5ae360ed66cd37f3a257b4585dacf82397633d
paul.patault <p.patault@gmail.com>
2018-10-18 16:24:54 +0200

    x

--- General/P6.01.py
+++ General/P6.01.py
@@ -1,30 +1,9 @@
 a = 1
 sc = 0
 cs = 0
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
 while a <= 100 :
     cs += a
     sc += a**2
     a += 1
 cs = cs ** 2
-<<<<<<< HEAD
-print ('somme des carrés', sc)
-print ('carré de la somme', cs)
-
-dif = cs-sc
-
-print ('\nDifférence "la somme des carrés" - "le carré de leur somme" est', dif )
-
-
-
-while i < int(len(n)-1):
-    if int(n[i])*int(n[i+1]) > a:
-        a = int(n[i])*int(n[i+1])
-    i += 1
-print ("\n", a)
-=======
 print (cs-sc)
->>>>>>> origin/master
